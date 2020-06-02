d9d40feb9747a89aee987388edac365a28581bae
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2020-06-02 00:36:14 -0500

    delete old files

--- profiler2/app.js
+++
@@ -1,4 +0,0 @@
-var file_listing = 'http://' + window.location.hostname + '/profiler/';
-console.log(file_listing)
-var link = document.getElementById("file_listing");
-link.setAttribute('href', file_listing);
